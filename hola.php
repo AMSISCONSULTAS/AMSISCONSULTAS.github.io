5ffa9a0c7f51273b81c894c6a4dcf13b220ddb52
AMSISCONSULTAS <brianchi19@gmail.com>
2018-09-23 20:07:15 -0500

    Update hola.php

--- hola.php
+++ hola.php
@@ -1,3 +1,13 @@
-<?php
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Ejemplo</title>
+    </head>
+    <body>
+
+        <?php
             echo "¡Hola, soy un script de PHP!";
-?>
+        ?>
+
+    </body>
+</html>
